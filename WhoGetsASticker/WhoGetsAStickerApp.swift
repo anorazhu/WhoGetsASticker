2f0bf649105ff8082e8c1cd20db017c8b003f756
anorazhu <zhusm@bc.edu>
2024-09-23 17:18:18 -0400

    Initial Commit

---
+++ WhoGetsASticker/WhoGetsAStickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoGetsAStickerApp.swift
+//  WhoGetsASticker
+//
+//  Created by Anora Zhu on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct WhoGetsAStickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
