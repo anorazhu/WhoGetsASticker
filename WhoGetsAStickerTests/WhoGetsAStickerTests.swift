a41ac2c1cbbc6761e4c16a6a42657d37dcbec40d
anorazhu <zhusm@bc.edu>
2024-09-23 17:18:18 -0400

    Initial Commit

---
+++ WhoGetsAStickerTests/WhoGetsAStickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhoGetsAStickerTests.swift
+//  WhoGetsAStickerTests
+//
+//  Created by Anora Zhu on 9/23/24.
+//
+
+import Testing
+@testable import WhoGetsASticker
+
+struct WhoGetsAStickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
